97061d4013824821d34a364d39d0d0aee98be5bb
github username <lluisguardiola.h@gmail.com>
2020-01-15 17:21:12 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (arr) => {
+    return arr.slice(0,2)
+}
+
+const returnLastTwoDrivers = (arr) => {
+    return arr.slice(-2)
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers, 
+    returnLastTwoDrivers
+]
+
+const createFareMultiplier = (multiplier) => {
+    return (value) => {
+        return value*multiplier
+    }
+}
+
+
+const fareDoubler = (fare) => {
+    return fare*2
+}
+
+const fareTripler = (fare) => {
+    return fare*3
+}
+
+const selectDifferentDrivers = (arr, fn) => {
+    if (fn === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(arr)
+    } else {
+        return returnLastTwoDrivers(arr)
+    }
+}
